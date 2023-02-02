be7b68d1008e37c7a51a87c11511cd0340f248d0
Nany2911 <110212209+Nany2911@users.noreply.github.com>
2023-02-02 23:36:45 +0530

    Add files via upload
    pwskills

---
+++ Untitled1.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+1=[Guido-van_Rossum]
+
+
+# In[ ]:
+
+
+2=[object-oriented, structured, and functional programming]
+
+
+# In[ ]:
+
+
+3=[lowercase] 
+
+
+# In[ ]:
+
+
+4=[.py]
+
+
+# In[ ]:
+
+
+5=[interpreted]
+
+
+# In[ ]:
+
+
+if (condition):
+    # Statement 1
+    # Statement 2
+    # Statement 3
+else:
+    # Statement 4
+    # Statement 5
+    # Statement 6
+
+
+# In[ ]:
+
+
+[7]=using in (#)
+
+
+# In[ ]:
+
+
+[8]=python(3.7)
+
+
+# In[ ]:
+
+
+[9]=lambda
+
+
+# In[ ]:
+
+
+[10]=Python Package Index
+
+
+# In[ ]:
+
+
+[11]=(isinstance,zip,map)
+
+
+# In[ ]:
+
+
+[12]=(79)
+
+
+# In[ ]:
+
+
+[13]=(Easy to learn, Availability of support,Large global community)
+
+
+# In[ ]:
+
+
+[14]=(garbage collection)
+
+
+# In[ ]:
+
+
+[15]=(System Environment Variable Path)
+
+
+# In[ ]:
+
+
+[16]=(yes)
+
